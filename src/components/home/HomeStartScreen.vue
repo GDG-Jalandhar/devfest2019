e2888d303162082c7ecbba44850b74e138c92e41
Vrijraj Singh <vrijraj2396@gmail.com>
2019-07-27 02:11:07 +0530

    Home Sub compo

---
+++ src/components/home/HomeStartScreen.vue
@@ -0,0 +1,56 @@
+<template>
+    <v-container class="pa-0">
+        <v-layout wrap align-center justify-center row fill-height  >
+           <v-flex xs12 sm5 md3 lg3 xl3 class="text-xs-center pa-5">
+                <v-img
+                    :src="require('@/assets/img/devfest-logo.jpg')"
+                    :lazy-src="require('@/assets/img/devfest-logo.jpg')"
+                    width="100%">
+                    <v-layout
+                        slot="placeholder"
+                        fill-height
+                        align-center
+                        justify-center
+                        ma-0
+                    >
+                        <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                    </v-layout>
+                </v-img>
+            </v-flex> 
+            <v-flex xs12 sm7 md8 lg8 xl8 class="pa-5">
+                <p class="google-font mb-1" style="font-weight: 350;color: #616161;font-size:300%"><b>DevFest  
+                    <span style="color: #1a73e8;">Punjab</span>.</b> 
+                </p>
+                <!-- <p class="google-font mb-1 " style="font-weight: 350;color: #616161;font-size:300%"><b>Be a  
+                    <span style="color: #1a73e8;">hero</span>.</b> <b> Be a <span style="color: #1a73e8;">GDG</span>.</b>
+                </p> -->
+                <p class="google-font mt-0" style="font-size:180%">GDG Jalandhar <v-icon>mdi-unfold-more-vertical mdi-36px</v-icon> GDG Chandigarh</p>
+                <p class="google-font" style="font-size:110%">DevFests are community-led, developer events hosted by GDG chapters around the globe focused on community building and learning about Google’s technologies. </p>
+                <p class="google-font" style="font-size:110%;color:#9e9e9e">
+                    <span v-for="(item,i) in chapterDetails.ChapterHashTags" :key="i">
+                        #{{item}} &nbsp;
+                    </span>
+                </p>
+
+                <v-btn :href="chapterDetails.ChapterMeetupLink" target="_blank" class="ma-0 google-font elevation-1" color="#1a73e8" style="text-transform: capitalize;border-radius:5px;color:white">Request for an Invitation</v-btn>
+
+                &nbsp;
+
+                <v-btn :href="chapterDetails.GDGProgramWebsite" target="_blank" rounded color="cyan" style="text-transform: capitalize;border-radius:5px;text-transform: capitalize;" outlined  class="ml-0" dark>Learn More</v-btn>
+
+            </v-flex>
+        </v-layout>
+
+    </v-container>
+</template>
+
+<script>
+import ChapterDetails from '@/assets/data/chapterDetails.json'
+export default {
+    data() {
+        return {
+            chapterDetails: ChapterDetails
+        }
+    },
+}
+</script>
