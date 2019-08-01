88fcb09384c2ea5d9c59a19c3b8dcd681e320a65
Vrijraj Singh <vrijraj2396@gmail.com>
2019-08-02 00:45:35 +0530

    store vuex updated

--- src/store.js
+++ src/store.js
@@ -5,10 +5,24 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    drawer: false,
+    items: [ 
+      { text: 'Home', to: '/', icon:'mdi-home'},
+      { text: 'Schedule', to: '/schedule', icon: 'mdi-format-float-left'},
+      { text: 'Speakers', to: '/speakers', icon:'mdi-account-switch'},
+      { text: 'Team', to: '/team', icon:'mdi-account-settings'},
+      { text: 'About', to: '/about', icon: 'mdi-note-multiple-outline'},
+      { text: 'Contact', to: '/contact', icon:'mdi-contacts'}
+    ]
+  },
+  getters:{
+    links: (state) => {
+      return state.items
+    }
   },
   mutations: {
-
+    setDrawer: (state, payload) => (state.drawer = payload),
+    toggleDrawer: state => (state.drawer = !state.drawer)
   },
   actions: {
 
