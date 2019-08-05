291475c5c7b0e5b87adae673a0e7b145df27b000
Vrijraj <vrijraj2396@gmail.com>
2019-08-05 16:52:29 +0530

    Speakers c Updated

--- src/views/Speakers.vue
+++ src/views/Speakers.vue
@@ -1,3 +1,22 @@
 <template>
-    <h1>Speakers Page</h1>
+    
+    <div>
+        <SpeakerHeader class="pa-0"/>
+        <comingSoon class="my-5 pa-4"/>
+        <br>
+        <!-- <SpeakerDetails/> -->
+    </div>
 </template>
+
+<script>
+import SpeakerHeader from '@/components/speakers/header'
+import SpeakerDetails from '@/components/speakers/speakers'
+import comingSoon from '@/components/speakers/comingSoon'
+export default {
+    components:{
+        SpeakerHeader,
+        SpeakerDetails,
+        comingSoon
+    }
+}
+</script>
