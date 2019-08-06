3d06cac2e108c72972167e3a52928b87de48fa49
Vrijraj <vrijraj2396@gmail.com>
2019-08-06 16:02:08 +0530

    Updated

---
+++ src/components/speakers/header.vue
@@ -0,0 +1,58 @@
+<template>
+    <v-container class="grey lighten-5 px-3" style="border:1px solid #9e9e9e;border-radius:7px">
+        <v-layout wrap align-center justify-center row fill-heights>
+
+            <v-flex xs12 sm5 md6 lg6 xl6 order-xs2 class="pa-5">
+               <p class="google-font mb-1 hidden-sm-and-down" style="font-weight: 350;color: #616161;font-size:200%"><b>Our   
+                    <span style="color: #1a73e8;">Speakers</span></b> 
+                </p>
+                <p class="google-font mt-3">
+                    Being the tech leaders from across the region, also they are the same community people, just Iike us! Attend them in person and listen to their expert talks and know more about them below.
+                </p>
+                
+                <v-btn href="#" color="#1a73e8" target="_blank" rounded style="text-transform: capitalize;border-radius:5px;text-transform: capitalize;"  class="ml-0 google-font mt-2" dark>Call For Speakers</v-btn>
+
+                <p class="google-font mt-2">* Last Date for apply: 15th Sept 2019</p>
+
+            </v-flex> 
+
+            <v-flex xs12 sm7 md6 lg6 xl6 order-xs1 class="">
+                    <v-img
+                        aspect-ratio="1"
+                        style="border:1px solid #9e9e9e;border-top-right-radius:7px;border-bottom-right-radius:7px"
+                        class="white--text"
+                        max-height="300"
+                        gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                        :src="'https://storage.googleapis.com/io-19-assets/images/homepage/intro_recap_1x.jpg'"
+                        :lazy-src="'https://storage.googleapis.com/io-19-assets/images/homepage/intro_recap_1x.jpg'"
+                        >
+                        <v-card-title
+                            class="fill-height align-end google-font"
+                        >
+                            Speakers
+                        </v-card-title>
+                        <v-layout
+                            slot="placeholder"
+                            fill-height
+                            align-center
+                            justify-center
+                            ma-0
+                        > 
+                            <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                        </v-layout>
+                    </v-img>
+            </v-flex> 
+        </v-layout>
+
+
+    </v-container>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+        }
+    },
+}
+</script>
---
+++ src/components/speakers/speakers.vue
@@ -0,0 +1,81 @@
+<template>
+    <v-container class="mb-0 pb-0">
+        <v-layout wrap align-center justify-center row fill-height>
+            <v-flex xs12 class="px-5">
+                <v-layout row wrap class="pa-0">
+                    <v-flex xs6 sm3 md3 lg2 v-for="(item,i) in speakerData" :key="i" 
+                    class="pa-1" style="text-align:center">
+                        
+                        <v-hover>
+                        <div slot-scope="{ hover }"
+                            style="border-radius: 5px; border:1px solid white"
+                            :class="`elevation-${hover ? 0 : 0}`"
+                            class="text-xs-center ma-1 pa-1 py-3 my-0" >
+                            <v-avatar size="100">
+                                <v-img
+                                :src="getImgUrl(item.profileImage)"
+                                :lazy-src="getImgUrl(item.profileImage)">
+
+                                    <v-layout
+                                        slot="placeholder"
+                                        fill-height
+                                        align-center
+                                        justify-center
+                                        ma-0
+                                    >
+                                        <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                                    </v-layout>
+                                
+                                </v-img>
+                            </v-avatar>
+                            <p class="mt-3 mb-0 google-font" style="font-size:130%">{{item.name}}</p>
+                            <p class="mt-1 mb-0 google-font">{{item.designation}}</p>
+
+                            <v-btn small class="mt-0 mx-0" text icon v-if="(item.twitter).length>0" :href="item.twitter" target="_blank">
+                                <v-icon small style="color:#1da1f2">mdi-twitter</v-icon>
+                            </v-btn>
+
+                            <v-btn class="mt-0 mx-0" small text icon v-if="(item.linkedin).length>0" :href="item.linkedin" target="_blank">
+                                <v-icon small style="color:#0077b5">mdi-linkedin</v-icon>
+                            </v-btn>
+
+                            <v-btn class="mt-0 mx-0" small text icon v-if="(item.github).length>0" :href="item.github" target="_blank">
+                                <v-icon small style="color:#333">mdi-github-circle</v-icon>
+                            </v-btn>
+                            <v-btn class="mt-0 mx-0" small text icon v-if="(item.website).length>0" :href="item.website" target="_blank">
+                                <v-icon small style="color:#333">mdi-link</v-icon>
+                            </v-btn>
+                            <!-- <SpeakerDetailsPopUp :data="{popState:popUpState}"/> -->
+                            
+                        </div>
+                        </v-hover>
+
+                        
+                    </v-flex>    
+                </v-layout> 
+            </v-flex>
+        </v-layout>
+
+
+    </v-container>
+</template>
+
+<script>
+import speakerData from '@/assets/data/speaker.json'
+export default {
+    data() {
+        return {
+            speakerData: speakerData
+        }
+    },
+    methods:{
+        getImgUrl(pic) {
+            if(pic.length>0){
+                return require('@/assets/img/speakers/'+pic)
+            }else{
+                return require('@/assets/img/common/avatar.png')
+            }
+        },
+    }
+}
+</script>
