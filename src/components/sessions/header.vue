ecd840614f59d249a00d03d0e684babf5f82a267
Vrijraj Singh <vrijraj2396@gmail.com>
2019-08-08 01:32:42 +0530

    Session header c updated

--- src/components/sessions/header.vue
+++ src/components/sessions/header.vue
@@ -1,10 +1,10 @@
 <template>
     <!-- style="border:1px solid #9e9e9e;border-radius:7px" -->
-    <v-container class=" px-3" >
+    <v-container class="px-3" >
         <v-layout wrap align-center justify-center row fill-heights>
 
-            <v-flex xs12 sm12 md12 lg12 xl12 order-xs2 order-md1 class="pa-5">
-               <p class="google-font mb-1 hidden-sm-and-down" style="font-weight: 350;color: #616161;font-size:200%"><b>Our   
+            <v-flex xs12 sm12 md12 lg12 xl12 order-xs2 order-md1 class="px-5 pt-5 mx-3">
+               <p class="google-font mb-1" style="font-weight: 350;color: #616161;font-size:200%"><b>Our   
                     <span style="color: #1a73e8;">Sessions</span></b> 
                 </p>
                 <p class="google-font mt-3">
