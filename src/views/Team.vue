a3f74da8bbec05e786789d642037327931b8d4ea
Vrijraj Singh <vrijraj2396@gmail.com>
2019-08-10 02:31:48 +0530

    team c updated

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,10 +1,9 @@
 <template>
   <v-content class="pa-0">
-
-    <v-container fluid class="mt-3 mb-0">
+    <v-container fluid class="mb-4">
       <v-layout wrap align-center justify-center row fill-height class="my-0">
         <v-flex xs12 md10 class="my-0 pa-5">
-            <p class="google-font mb-0" style="font-size:200%;color:#616161">Our lineup of big thinkers and even bigger doers</p>
+            <p class="google-font mb-0" style="font-size:200%;color:#1a73e8">Our lineup of big thinkers and even bigger doers</p>
             <p class="google-font mt-0 mb-0" style="font-size:110%">Google is known all around the world. Everyone is 'googling', checking on 'maps' and communicating in 'gmail'. For simple users, they are services that just works, but not for us. Developers see much more: APIs, scalability issues, complex technology stacks. And that is what GDG is about. </p>
 
             <p class="google-font mt-3 mb-0" style="font-size:110%">Our goal is to organize space to connect the best industry experts with Indian audience to boost development of IT. And Our Core Team is: </p>
@@ -15,7 +14,7 @@
 
     <v-container fluid >
       <v-layout wrap align-center justify-center row fill-height>
-        <v-flex xs12 md10 class="">
+        <v-flex xs12 md10>
           <coreTeam/>
         </v-flex>
       </v-layout>
@@ -23,7 +22,7 @@
 
     <v-container fluid >
       <v-layout wrap align-center justify-center row fill-height>
-        <v-flex xs12 md10 class="pa-5">
+        <v-flex xs12 md10>
           <organizingTeam/>
         </v-flex>
       </v-layout>
