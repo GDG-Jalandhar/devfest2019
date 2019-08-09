d6ff0750a758ad875db831339a4c4c5d80587089
Vrijraj Singh <vrijraj2396@gmail.com>
2019-08-10 02:46:43 +0530

    link icon u

--- src/components/common/socialMedia.vue
+++ src/components/common/socialMedia.vue
@@ -12,7 +12,7 @@
             <v-icon small style="color:#333">fab fa-github</v-icon>
         </v-btn>
         <v-btn small class="mt-0 mx-0" icon v-if="(data.vdata.website).length>0" :href="data.vdata.website" target="_blank">
-            <v-icon style="color:#333">link</v-icon>
+            <v-icon style="color:#333">mdi-link</v-icon>
         </v-btn>
     </div>
 </template>
