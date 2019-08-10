d640fdd489d2b3257f211cecda9efbf35b5d7efd
Vrijraj Singh <vrijraj2396@gmail.com>
2019-08-10 14:26:02 +0530

    aoo c u

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,31 @@
 <template>
   <v-app>
+     <v-snackbar
+        v-model="snackWithButtons"
+        :timeout="timeout"
+        bottom
+        left
+        class="snack"
+      >
+        {{ snackWithBtnText }}
+        <v-spacer />
+        <v-btn
+          dark
+          text
+          color="#00f500"
+          @click.native="refreshApp"
+        >
+          {{ snackBtnText }}
+        </v-btn>
+        <v-btn
+          icon
+          @click="snackWithButtons = false"
+        >
+          <v-icon>mdi-close-circle-outline</v-icon>
+        </v-btn>
+    </v-snackbar>
+
+
     <!-- v-if="!$route.meta.hideNavigation" -->
     <CoreToolbar />
     <CoreDrawer/>
@@ -28,7 +54,41 @@ export default {
     CoreDrawer
   },
   data: () => ({
-    //
+    refreshing: false,
+    registration: null,
+    snackBtnText: '',
+    snackWithBtnText: '',
+    snackWithButtons: false,
+    timeout: 6000,
   }),
-};
+   created() {
+    // Listen for swUpdated event and display refresh snackbar as required.
+    document.addEventListener('swUpdated', this.showRefreshUI, { once: true });
+    // Refresh all open app tabs when a new service worker is installed.
+    navigator.serviceWorker.addEventListener('controllerchange', () => {
+      if (this.refreshing) return;
+        this.refreshing = true;
+      window.location.reload();
+    });
+  },
+  methods:{
+    showRefreshUI(e) {
+      this.registration = e.detail;
+      this.snackBtnText = 'Refresh';
+      this.snackWithBtnText = 'New version available!';
+      this.snackWithButtons = true;
+    },
+    refreshApp() {
+      this.snackWithButtons = false;
+      if (!this.registration || !this.registration.waiting) { return; }
+      this.registration.waiting.postMessage('skipWaiting');
+    }
+  }
+}
 </script>
+
+<style scoped>
+.snack >>> .v-snack__content {
+  padding-right: 16px;
+}
+</style>
