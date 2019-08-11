2962913424bcc0e764273829e9602743d8a35f04
Vrijraj Singh <vrijraj2396@gmail.com>
2019-08-11 18:59:47 +0530

    view file update

--- src/components/common/view.vue
+++ src/components/common/view.vue
@@ -1,7 +1,7 @@
 <template>
   <v-content class="white">
     <v-slide-y-reverse-transition>
-      <router-view v-show="show" />
+      <router-view v-show="show"  />
     </v-slide-y-reverse-transition>
   </v-content>
 </template>
