f62e99a1b2740bce54cebf36ab3d06f18b2f5df2
varsha-jaiswal <shaluvarshajaiswal@gmail.com>
2019-09-03 16:06:02 +0530

    Spelling mistake improved

--- src/components/home/stats.vue
+++ src/components/home/stats.vue
@@ -3,10 +3,10 @@
         <v-layout wrap align-center justify-center row fill-height>
             <v-flex xs12 sm5 md6 lg6 xl6 class="pa-5">
                <p class="google-font mb-1" style="font-weight: 350;color: #616161;font-size:200%"><b>On the edge of  
-                    <span style="color: #1a73e8;">Android, Web</span> and <span style="color: #1a73e8;">Cloud</span></b> 
+                    <span style="color: #1a73e8;">Android, ML, Web</span> and <span style="color: #1a73e8;">Cloud</span></b> 
                 </p>
                 <p class="google-font mt-3">
-                    GDG DevFest Jalandhar brings together people who shape the future of Android, Web and Cloud technologies for 2 days of sessions, workshops and networking. Described by many as 'Google I/O of Jalandhar', our team creates DevFest to be the best place for inspiration and experience sharing.
+                    GDG DevFest Punjab brings together people who shape the future of Android, ML, Web and Cloud technologies with a day full of sessions, workshops and networking. Described by many as 'Google I/O of Punjab', our team creates DevFest to be the best place for inspiration and experience sharing.
                 </p>
                 
                 <v-btn href="#" color="#1a73e8" target="_blank" rounded style="text-transform: capitalize;border-radius:5px;text-transform: capitalize;" outlined  class="ml-0 google-font mt-3" dark>How it was in 2018</v-btn>
@@ -20,7 +20,7 @@
                             <span style="color: #1a73e8;">350+</span></b> 
                         </p>
                         <p class="google-font">
-                            Attendeess
+                            Attendees
                         </p>
                     </v-flex>
 
@@ -29,7 +29,7 @@
                             <span style="color: #1a73e8;">3+</span></b> 
                         </p>
                         <p class="google-font">
-                            Workshops
+                            Workshops/Codelabs
                         </p>
                     </v-flex>
                     <v-flex xs6>
